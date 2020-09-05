485be39e10adedecc1c66a99e063bc1da428be95
Moran96 <liumoran@outlook.com>
2020-09-05 18:22:23 +0800

    自定义serve

--- app/router.js
+++ app/router.js
@@ -6,4 +6,6 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+  // 使用 Service
+  app.router.get('/user/:id', app.controller.user.info);
 };
---
+++ app/service/user.js
@@ -0,0 +1,24 @@
+const Service = require('egg').Service
+
+class UserService extends Service {
+    async find (uid) {
+        // const user = await this.ctx.db.query('select * from user where uid = ?', uid)
+        const user = {
+            user_name: 'aaaa',
+            age: 33
+        }
+        const picture = await this.getPicture(uid)
+        return  {
+            name: user.user_name,
+            age: user.age,
+            picture
+        }
+    }
+
+    async getPicture(uid) {
+        const result = await this.ctx.curl(`http://photoserver/uid=${uid}`, { dataType: 'json' })
+        return result.data
+    }
+}
+
+module.exports = UserService
